dcbe934631101d4871f4211a317b7290387cae8d
first1 <amalm9479@gmail.com>
2021-03-05 04:14:53 +0300

    first

---
+++ app/src/main/java/com/amalm9479/coderswag/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amalm9479.coderswag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
